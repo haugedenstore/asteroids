f43118cf1ee79cc0e8210c38bd5359a56df72231
Bjørn Hauge <haugedenstore@gmail.com>
2026-06-29 12:44:53 +0200

    L10/20 asteroids

--- hauges-asteroids/main.py
+++ hauges-asteroids/main.py
@@ -1,16 +1,25 @@
+import os
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
-    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    os.environ["SDL_VIDEO_CENTERED"] = "1"
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0.0
         
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    pygame.display.set_caption("Asteroids")
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(player.triangle())
     while True:
         
         log_state()
@@ -21,7 +30,12 @@ def main():
         
         screen.fill("black")
         
+        player.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+        
     
     
 if __name__ == "__main__":
