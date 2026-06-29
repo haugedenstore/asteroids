0cc9315c02f6ce43a8b77b5428430d4f4f16e011
haugedenstore <haugedenstore@gmail.com>
2026-06-29 21:55:02 +0200

    added left/right rotation for player class

--- hauges-asteroids/main.py
+++ hauges-asteroids/main.py
@@ -16,10 +16,9 @@ def main():
     dt = 0.0
         
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    print(player.triangle())
+    
     while True:
         
         log_state()
@@ -31,11 +30,10 @@ def main():
         screen.fill("black")
         
         player.draw(screen)
-        
+        player.update(dt)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
-        
     
     
 if __name__ == "__main__":
