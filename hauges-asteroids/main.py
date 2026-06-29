8593a8e529668f588d17ff44fdff2829edac4f7a
haugedenstore <haugedenstore@gmail.com>
2026-06-29 22:32:45 +0200

    added asteroids, asteroid field grou p and instantiated it.

--- hauges-asteroids/main.py
+++ hauges-asteroids/main.py
@@ -1,5 +1,7 @@
 import os
 import pygame
+from asteroidfield import AsteroidField
+from asteroids import Asteroid
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -17,20 +19,30 @@ def main():
         
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
     
     while True:
         
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
-        
-        player.draw(screen)
-        player.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
