ed4c68de639a3c0a64b3bbea83b0f75762863e07
haugedenstore <haugedenstore@gmail.com>
2026-07-01 00:04:18 +0200

    added collision detection and .kill() for asteroid-shot hits

--- hauges-asteroids/main.py
+++ hauges-asteroids/main.py
@@ -50,6 +50,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
